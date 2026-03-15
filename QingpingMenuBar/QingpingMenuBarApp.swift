62f3205e7b994f4b0f22c4d0fe0f7abbcaf6661d
Andreu Gordillo <andreugordillovazquez@gmail.com>
2026-03-15 23:35:26 +0100

    Initial Commit

---
+++ QingpingMenuBar/QingpingMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  QingpingMenuBarApp.swift
+//  QingpingMenuBar
+//
+//  Created by Andreu Gordillo Vázquez on 15/3/26.
+//
+
+import SwiftUI
+
+@main
+struct QingpingMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
